dc358cb458ba6539baf148198987debfcf464e95
ielmie <65829854+ielmie@users.noreply.github.com>
2020-07-21 15:16:42 -0400

    Version 1 of the Trivia App
    The initial version will ask you 10 questions, tell you if you are correct between each question, and then return you to the main screen. The questions are imported from the triviaQuestions.txt file but are not necessarily different from each other (the next feature to be implemented).
    
    The files provided: 
    - From the src folder of the project and include the original code (mainActivity.java, Question.java, and Results.java), as well as some generated by android studio.
    - The compiled application which can be run on an emulator (for best results please use a Pixel virtual machine running API30) app-debug.apk

---
+++ main/java/com/example/triviaapp/MainActivity.java
@@ -0,0 +1,47 @@
+//Author: Émilie Fortin
+
+package com.example.triviaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+   Button quitButton, startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initializes the quit Button
+        //When clicked it causes the app to close gracefully
+        quitButton = (Button) findViewById(R.id.quitButton);
+        quitButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                System.exit(0);
+            }
+        });
+
+        //Initializes the start Button
+        //When clicked it causes the app to go to the quiz activity
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startQuiz();
+            }
+        });
+
+    }
+
+    public void startQuiz(){
+        Intent intent = new Intent(this, Question.class);
+        intent.putExtra("numberOfQuestions",0);
+        intent.putExtra("numberOfCorrectAnswers",0);
+        intent.putExtra("questionNumbers", new int[10]);
+        startActivity(intent);
+    }
+}
---
+++ main/java/com/example/triviaapp/Results.java
@@ -0,0 +1,81 @@
+//Author: Émilie Fortin
+
+package com.example.triviaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Results extends AppCompatActivity {
+
+    Button buttonContinue;
+    TextView resultScreen;
+
+    int questionNum;
+    int numberOfCorrectAnswers;
+    int[] questionOrder;
+    boolean resultsCorrect;
+    String correct = "correct";
+    String inCorrect = "Incorrect";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+        // retrieves the data transmitted to it when it passes from one activity to the other
+        Intent intent = getIntent();
+        numberOfCorrectAnswers = intent.getIntExtra("numberOfCorrectAnswers",0);
+        questionNum = intent.getIntExtra("numberOfQuestions",0);
+        questionOrder = intent.getIntArrayExtra("questionNumbers");
+        resultsCorrect = intent.getBooleanExtra("isCorrect", true);
+
+        //Initializes the textView and Button and assigns the text to each
+        resultScreen = (TextView) findViewById(R.id.resultScreen);
+        if(resultsCorrect){
+            resultScreen.setText(correct);
+        }
+        if(!resultsCorrect){
+            resultScreen.setText(inCorrect);
+        }
+
+
+        buttonContinue = (Button) findViewById(R.id.buttonContinue);
+        buttonContinue.setText("Continue");
+
+        //The code for when button is clicked.
+        //The buttons will change the display back to the question page if there have not been 10 questions left
+        // or to the main page if 10 questions have been reached
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(questionNum>=10){
+                    returnToMenu();
+                }
+                else{
+                    startNextQuestion();
+                }
+            }
+        });
+
+
+    }
+
+    public void returnToMenu(){
+        Intent in = new Intent(this, MainActivity.class);
+        startActivity(in);
+    }
+
+    public void startNextQuestion(){
+        Intent intents = new Intent(this, Question.class);
+        intents.putExtra("numberOfQuestions", questionNum);
+        intents.putExtra("numberOfCorrectAnswers",numberOfCorrectAnswers);
+        intents.putExtra("questionNumbers", questionOrder);
+        startActivity(intents);
+    }
+
+
+}
