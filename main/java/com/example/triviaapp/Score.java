9ff35033c1b06052e55e78ada1c31ba230ded670
ielmie <65829854+ielmie@users.noreply.github.com>
2020-07-21 21:23:52 -0400

    version 3
    This version will ask you 10 questions, telling you if you are correct between each question. Then it will bring you to an activity which will allow you to see your score, then return you to the main screen. The questions are imported from the triviaQuestions.txt file and are different from each other.
    
    Changes:
    
        - A Score activity was added to bridge the gap between the end of the 10 questions and the return to the main menu.
        -The layout of all the activities have been updated to be more esthetically pleasing by making the text larger and centered.
    
    The files provided:
    
        From the src folder of the project and include the original code (mainActivity.java, Question.java, and Results.java), as well as some generated by android studio.
        The compiled application which can be run on an emulator (for best results please use a Pixel virtual machine running API30) app-debug.apk

---
+++ main/java/com/example/triviaapp/Score.java
@@ -0,0 +1,61 @@
+package com.example.triviaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Score extends AppCompatActivity {
+
+    Button buttonToContinue;
+    TextView scoreScreen;
+    int numberOfCorrectAnswers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        // retrieves the data transmitted to it when it passes from one activity to the other
+        Intent intent = getIntent();
+        numberOfCorrectAnswers = intent.getIntExtra("numberOfCorrect",0);
+
+        //Initializes the textview
+        scoreScreen = (TextView) findViewById(R.id.scoreScreen);
+        addScoreToTextView(numberOfCorrectAnswers,scoreScreen);
+
+        //Creates the continue button
+        //And the code for when the button is clicked
+        //which returns us to the main menu
+        buttonToContinue = (Button) findViewById(R.id.buttonToContinue);
+        buttonToContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnToMenu();
+            }
+        });
+
+    }
+
+    //Method that takes an integer and a Textview as input
+    //And assigns the appropriate score text to said textView
+    public void addScoreToTextView(int number, TextView tv){
+        if(number<6){
+
+            tv.setText("You got "+ Integer.toString(number)+"/10"+" Better luck next time!");
+        }
+        else{
+            tv.setText("You got "+ Integer.toString(number)+"/10"+" Congratulations!");
+        }
+    }
+
+    //Method that creates an intent to return to the main menu
+    //then starts that activity
+    public void returnToMenu(){
+        Intent in = new Intent(this, MainActivity.class);
+        startActivity(in);
+    }
+}
